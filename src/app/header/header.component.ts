9a92c2d37a6889ce45b20960bc80272f48874d24
meng-Shiun <lin.mengshiun@gmail.com>
2018-10-02 10:13:15 +0200

    feat: Add Navigation with Event Binding and ngIf

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,14 @@
-import { Component } from "@angular/core";
+import { Component, Output, EventEmitter } from "@angular/core";
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+    @Output() featureSelected: EventEmitter<string> = new EventEmitter();
+
+    onSelect(feat: string) {
+        this.featureSelected.emit(feat);
+    }
 
 }
