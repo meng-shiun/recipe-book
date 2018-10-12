3f4608a345184f6a4723eddb1c32a1b93ddbcb93
meng-Shiun <lin.mengshiun@gmail.com>
2018-10-12 12:45:39 +0200

    feat: Add ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,30 @@
 import { EventEmitter } from "@angular/core";
 
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
 
     recipeSelected = new EventEmitter<Recipe>();
     
     private recipes: Recipe[] = [
-        new Recipe('Cheese burger', 'Cheesy and yummy',
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/800px-RedDot_Burger.jpg'),
-        new Recipe('Pasta', 'Home made pasta', 'https://res.cloudinary.com/norgesgruppen/image/upload/c_fill,f_auto,h_574,q_80,w_945/ayzgfdbzwndgnpfwgzf9.jpg')
+        new Recipe(
+            'Cheese burger', 
+            'Cheesy and yummy',
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/800px-RedDot_Burger.jpg',
+            [
+                new Ingredient('Chesse', 1),
+                new Ingredient('Meat', 1),
+                new Ingredient('Buns', 2)
+            ]),
+        new Recipe(
+            'Pasta', 
+            'Home made pasta', 
+            'https://res.cloudinary.com/norgesgruppen/image/upload/c_fill,f_auto,h_574,q_80,w_945/ayzgfdbzwndgnpfwgzf9.jpg',
+            [
+                new Ingredient('Pasta', 20),
+                new Ingredient('Chicken', 3)
+            ])
     ];
 
     getRecipes = () => this.recipes.slice();
