4c955a76e2c9caf1c4498e6548dd3888bc7e69ed
meng-Shiun <lin.mengshiun@gmail.com>
2018-10-12 14:50:22 +0200

    feat: Pass ingredients to Shopping List

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,8 +1,10 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 
 import { Recipe } from "./recipe.model";
 import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
 
     recipeSelected = new EventEmitter<Recipe>();
@@ -27,5 +29,11 @@ export class RecipeService {
             ])
     ];
 
+    constructor(private shoppingListService: ShoppingListService) {}
+
     getRecipes = () => this.recipes.slice();
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.shoppingListService.addIngredients(ingredients);
+    }
 }
