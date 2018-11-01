5cfbd1afb7d70dad06a2958b8fb889cd444146bf
meng-Shiun <lin.mengshiun@gmail.com>
2018-11-01 14:42:24 +0100

    feat: Set up Firebase SDK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'recipe-book';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCstlNl1FNDXtz7mrCWbO-6nGj7Vu4moCc",
+      authDomain: "recipe-book-ms.firebaseapp.com"
+    });
+  }
 }
