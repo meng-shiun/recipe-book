81f156e8a96cd01864fdadde52702392623afb5b
meng-Shiun <lin.mengshiun@gmail.com>
2018-11-01 15:54:15 +0100

    feat: Sign Users up

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,13 @@
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
+@Injectable({
+    providedIn: 'root'
+})
 export class AuthService {
     signupUser(email: string, password: string) {
-        
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .catch(err => console.log(err));
     } 
 }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+import { AuthService } from '../auth.service';
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -8,14 +10,14 @@ import { NgForm } from '@angular/forms';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   onSignup(form: NgForm) {
     const { email, password } = form.value;
-    console.log('email:', email + ' || ' + 'password:', password);
+    this.authService.signupUser(email, password);
   }
 
 }
