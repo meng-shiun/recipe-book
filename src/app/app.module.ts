c2bff7ff330cc21557384a117fb6dd4dd0f2d364
meng-Shiun <lin.mengshiun@gmail.com>
2018-11-04 15:39:09 +0100

    feat: Create Auth Feature Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,25 +5,23 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 import { AuthGuard } from './auth/auth-guard.service';
 import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    SignupComponent,
-    SigninComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     RecipesModule,
     ShoppingListModule,
+    AuthModule,
     AppRoutingModule,
     SharedModule
   ],
